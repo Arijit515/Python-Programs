28f68d9c317f444efa9a3895837772758c1285d1
Arijit515 <arijitgamer728@gmail.com>
2021-11-03 15:53:43 +0530

    First Commit

---
+++ Prog_11.py
@@ -0,0 +1,4 @@
+num = int(input ("Enter the number to print the multiplication table: "))      
+print ("The Multiplication Table of: ", num)    
+for c in range(1, 11):      
+   print (num, 'x', c, '=', num * c)  
