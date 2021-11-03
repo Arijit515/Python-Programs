304ec3209fb03cc2f7ceddf267bf224011552571
Arijit515 <arijitgamer728@gmail.com>
2021-11-03 16:13:58 +0530

    Edition

--- Prog_13.py
+++ Prog_13.py
@@ -1,1 +1,8 @@
-n=int(input(Enter the number to print all even and odd terms:))
+n = int(input("Enter the number to display all even & odd terms: "))
+print ("odd Numbers:")
+for i in range(1,n,2):
+    print(i)
+print("--------------------------")
+print("Even Numbers:")
+for i in range(0,n,2):
+    print (i)    
